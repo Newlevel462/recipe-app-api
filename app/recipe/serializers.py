f69b031ee29f94535e63b3cdd275d19ede5903b5
newlevel462 <newlevel462@yahoo.com>
2022-02-10 12:42:58 +0000

    67. Implement feature for retrieving recipe detail

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -39,3 +39,9 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'name')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
